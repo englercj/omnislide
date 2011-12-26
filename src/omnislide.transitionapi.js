51140e3f0aa712e4d66d887097f6ae2ed73a383f
Chad Engler <englercj@live.com>
2011-12-26 13:16:01 -0500

    jshint fixes and optimizations

--- src/omnislide.transitionapi.js
+++ src/omnislide.transitionapi.js
@@ -144,29 +144,31 @@ $.OmniSlide.transitionAPI = {
             }
 
             if (i < len - 1) {
-                $box.delay((opt.delay * i), 'omnislide.transition')
-                    .queue('omnislide.transition', function (next) {
-                        if (opt.animation && $.isFunction(opt.animation)) {
-                            opt.animation.call(this, toCss, opt);
-                        } else {
-                            $(this).animate(toCss, opt.duration, opt.easing);
-                        }
-                        next();
-                    })
-                    .dequeue('omnislide.transition');
+                $box.data('animData', { opt: opt, toCss: toCss });
             } else {
-                j = (opt.order == 'randomize') ? $boxes.eq(0) : $boxes.eq(i);
-                $box.delay((opt.delay * i), 'omnislide.transition')
-                    .queue('omnislide.transition', function (next) {
-                        if (opt.animation && $.isFunction(opt.animation)) {
-                            opt.animation.call(this, toCss, opt, transitionDone);
-                        } else {
-                            $(this).animate(toCss, opt.duration, opt.easing, transitionDone);
-                        }
-                        next();
-                    })
-                    .dequeue('omnislide.transition');
+                //j = (opt.order == 'randomize') ? $boxes.eq(0) : $boxes.eq(i);
+                $box.data('animData', { opt: opt, toCss: toCss, cb: transitionDone });
+                //.delay((opt.delay * i), 'omnislide.transition')
+                //.queue('omnislide.transition', doAnim)
+                //.dequeue('omnislide.transition');
             }
+            $box.delay((opt.delay * i), 'omnislide.transition')
+                .queue('omnislide.transition', doAnim)
+                .dequeue('omnislide.transition');
+        }
+
+        function doAnim(next) {
+            var $this = $(this),
+                data = $this.data('animData');
+
+            if (!data) { $.OmniSlide.error('Error, animation data empty'); return; }
+
+            if (data.opt.animation && $.isFunction(data.opt.animation)) {
+                data.opt.animation.call(this, data.toCss, data.opt, data.cb);
+            } else {
+                $(this).animate(data.toCss, data.opt.duration, data.opt.easing, data.cb);
+            }
+            next();
         }
 
         function transitionDone() {
